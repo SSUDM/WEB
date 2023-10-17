453bd351032dd16d870d2897b5fe1b7523f8ca1b
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2023-10-17 13:57:19 +0900

    profile 페이지 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NewProject from "./routes/NewProject";
 import Project from "./routes/Project";
+import Profile from "./routes/Profile";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/newProject" element={<NewProject />} />
         <Route path="/project" element={<Project />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -34,6 +34,7 @@ const User = styled.div`
   span {
     font-weight: 700;
   }
+  margin-bottom: 40px;
 `;
 
 const UserImg = styled.img`
@@ -45,12 +46,37 @@ const UserImg = styled.img`
 `;
 
 const Tags = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
   width: 500px;
   border: solid 2px rgba(0, 0, 0, 0.1);
   border-radius: 20px;
+  padding: 20px 0;
+  margin-bottom: 30px;
 `;
 
-const Tag = styled.div``;
+const TagContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 15px;
+  h1 {
+    margin: 0;
+    font-size: 15px;
+    margin-right: 10px;
+  }
+  span {
+    font-size: 14px;
+  }
+`;
+
+const Tag = styled.div`
+  font-size: 14px;
+  font-weight: 300;
+  padding: 2px 10px;
+  background-color: rgba(0, 0, 0, 0.1);
+  border-radius: 20px;
+`;
 
 const Description = styled.div`
   h1 {
@@ -59,9 +85,12 @@ const Description = styled.div`
 `;
 
 const Contents = styled.div`
-  width: 500px;
-  height: 600px;
+  width: 480px;
+  height: 400px;
+  padding: 10px;
   border: solid 1px black;
+  border-left-color: white;
+  border-right-color: white;
 `;
 
 const Project = () => {
@@ -74,27 +103,30 @@ const Project = () => {
         <span>김OO</span>
       </User>
       <Tags>
-        <Tag>
-          <span>모집파트</span>
-        </Tag>
-        <Tag>
-          <span>모집 파트</span>
-        </Tag>
-        <Tag>
-          <span>모집 인원</span>
-        </Tag>
-        <Tag>
-          <span>기술 스택</span>
-        </Tag>
-        <Tag>
-          <span>개발 기간</span>
-        </Tag>
-        <Tag>
-          <span>모집 마감</span>
-        </Tag>
-        <Tag>
-          <span>요구 숙련도</span>
-        </Tag>
+        <TagContainer>
+          <h1>모집 파트</h1>
+          <Tag>프론트엔드</Tag>
+        </TagContainer>
+        <TagContainer>
+          <h1>모집 인원</h1>
+          <span>1명</span>
+        </TagContainer>
+        <TagContainer>
+          <h1>기술 스택</h1>
+          <Tag>react.js</Tag>
+        </TagContainer>
+        <TagContainer>
+          <h1>개발 기간</h1>
+          <span>2000.00.00 - 2000.00.00</span>
+        </TagContainer>
+        <TagContainer>
+          <h1>모집 마감</h1>
+          <span>2000.00.00</span>
+        </TagContainer>
+        <TagContainer>
+          <h1>요구 숙련도</h1>
+          <Tag>초급</Tag>
+        </TagContainer>
       </Tags>
       <Description>
         <h1>프로젝트 소개</h1>
