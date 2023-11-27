efa4c5757c604dcaa22e02a0453543ad3461e267
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2023-11-27 18:49:32 +0900

    [FIX] 네비게이션 바 css 추가

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,65 @@ import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { loginState } from "./atom";
 
+const NavbarStyle = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  /* font-size: 18px; */
+  font-family: "Pretendard-Regular";
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.1);
+`;
+const H1 = styled.h1`
+  width: 10%;
+  margin-right: 8%;
+  font-size: 35px;
+  font-weight: 700;
+  cursor: pointer;
+`;
+const LeftContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin-right: 15px;
+`;
+
+const RightContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin-left: 15px;
+`;
+const Menu = styled.p`
+  margin-left: 50px;
+  font-size: 14px;
+  cursor: pointer;
+`;
+const Login = styled.p`
+  cursor: pointer;
+  font-size: 14px;
+`;
+
+const CreateProject = styled.button`
+  border-radius: 40px;
+  border: 0;
+  font-weight: bold;
+  font-family: "Pretendard-Regular";
+  padding: 10px;
+  margin-left: 30px;
+  width: 150px;
+  background-color: #d2d2d2;
+  cursor: pointer;
+  &:hover {
+    outline: none;
+    border: 2px solid #9e9e9e;
+    background-color: #c8c8c8;
+    transform: scale(1.05);
+  }
+`;
+
 const Navbar = () => {
   const isLogin = useRecoilValue(loginState);
   const navigate = useNavigate();
